cfbb9b2f09a15d512e39c39556f143efc5b51d3d
parmod <parmodmagotra6391@gmail.com>
2023-09-25 11:58:24 +0530

    Added consult now card in Hero Section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,5 +1,6 @@
 import fieldsLogo1 from "../assets/images/doctors-fields-logo.png";
 import fieldsLogo2 from "../assets/images/doctors-fields-logo-2.png";
+import check from "../assets/images/tick-logo.png";
 
 const HeroSection = () => {
   return (
@@ -25,6 +26,33 @@ const HeroSection = () => {
         <div className="flex  mt-12">
           <img className="w-96" src={fieldsLogo1}></img>
           <img className="w-96" src={fieldsLogo2}></img>
+        </div>
+        <div className="w-[100vw] h-1 bg-slate-300"></div>
+        {/* TAGLINE COMPONENT */}
+        <div className="px-4 mt-16 shadow-xl rounded-md">
+          <h1 className="text-5xl ">
+            Skip the waiting Room. Consult with a Doctor
+          </h1>
+          {/* LIST ITMES */}
+          <div className="flex flex-col gap-5 w-6/12 mx-auto my-12">
+            <ul className=" flex flex-col gap-4 text-2xl">
+              <li className="flex font- ">
+                <img className="w-8" src={check}></img>
+                Fees starting at 500rs
+              </li>
+              <li className="flex ">
+                <img className="w-8" src={check}></img>
+                Verified Doctors Respond in 5 min.
+              </li>
+              <li className="flex ">
+                <img className="w-8" src={check}></img>
+                100% Private and Confidentail
+              </li>
+            </ul>
+            <button className="mt-4 text-green-500 border w-32 py-2 rounded-full px-4  border-green-500  ">
+              Consult now
+            </button>
+          </div>
         </div>
       </div>
     </div>
